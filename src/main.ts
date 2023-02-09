8b8a6a82282bfb2cf60868458af8860fadbffea2
Emmanuel Sarpong <emmanuel.sarpong@n26.com>
2023-02-09 08:56:00 +0100

    moved anteek module to root

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,25 @@
-require('dotenv').config();
+import { AnteekModule, Policies } from '@zicenter/anteek-core';
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+
+require('dotenv').config();
 
 async function bootstrap() {
-    const app = await NestFactory.create(AppModule);
+
+    const app = await NestFactory.create(
+        AnteekModule.forRoot({
+            resolvers: 'dist/**/*.resolver.js',
+            functions: 'dist/**/*.function.js',
+            middlewares: 'dist/**/*.middleware.js',
+            jwt: false,
+            disableGraphIntrospection: process.env.NODE_ENV === 'production',
+            policies: [
+                Policies.General.allowAll()
+            ],
+            imports: [],
+            providers: [],
+        })
+    );
+
     await app.listen(3000);
 }
 bootstrap();
